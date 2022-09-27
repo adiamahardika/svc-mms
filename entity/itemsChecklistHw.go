42f69fa6417150e63a9f51e86f15b82a39118401
Adia Mahardika <adiamahardika.work@gmail.com>
2022-09-27 14:08:41 +0700

    Add default status in items checklist hw

--- entity/itemsChecklistHw.go
+++ entity/itemsChecklistHw.go
@@ -3,8 +3,9 @@ package entity
 import "time"
 
 type ItemsChecklistHw struct {
-	Id        int       `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
-	CreatedBy string    `json:"created_by"`
-	CreatedAt time.Time `json:"created_at"`
+	Id            int       `json:"id" gorm:"primaryKey"`
+	Name          string    `json:"name"`
+	DefaultStatus string    `json:"status"`
+	CreatedBy     string    `json:"created_by"`
+	CreatedAt     time.Time `json:"created_at"`
 }
