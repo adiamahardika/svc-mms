ed46dbd608f31076950e0b8bb23fbbc0c734ed4f
Adia Mahardika <adiamahardika.work@gmail.com>
2022-09-30 10:04:47 +0700

    Change nama to items in items checklist hw

--- entity/itemsChecklistHw.go
+++ entity/itemsChecklistHw.go
@@ -4,7 +4,7 @@ import "time"
 
 type ItemsChecklistHw struct {
 	Id            int       `json:"id" gorm:"primaryKey"`
-	Name          string    `json:"name"`
+	Name          string    `json:"items"`
 	DefaultStatus string    `json:"status"`
 	CreatedBy     string    `json:"created_by"`
 	CreatedAt     time.Time `json:"created_at"`
